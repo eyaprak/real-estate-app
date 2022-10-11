4059be73b94e7b89ef5a7306aa4d10597ce6c4e9
eyaprak <yaprakemrullah22@gmail.com>
2022-10-11 18:22:15 +0300

    Head added for index

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
 import Content from "../components/Content";
-
+import Head from 'next/head';
 export default function Home() {
   return (
-    <Content />
+    <>
+      <Head>
+        <title>MyProperty Real Estate App</title>
+      </Head>
+      <Content />
+    </>
   )
 }
